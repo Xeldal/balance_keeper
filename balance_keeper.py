01687a685955ebd6565b6e29ab34db5a0b4b4123
Xeldal <xeldal@cryptolab.net>
2014-12-04 19:52:04 -0500

    Update balance_keeper.py

--- balance_keeper.py
+++ balance_keeper.py
@@ -113,7 +113,7 @@ while True:
         
         for i in response:
           for j in i.iteritems():
-            if j == 'ledger_entries':
+            if "ledger_entries" in j:
               for k in j.iteritems():
                 print(k)
        
