6938a791ba792b7c454f653720ce0c140aa53144
Xeldal <xeldal@cryptolab.net>
2014-12-04 21:01:42 -0500

    Update balance_keeper.py

--- balance_keeper.py
+++ balance_keeper.py
@@ -115,8 +115,8 @@ while True:
         k = 0
         for i in response:
           t = response[k]["ledger_entries"]
-            j = j["memo"]
-            print(j)
+          j = j["memo"]
+          print(j)
           k = k + 1
        
         ##feed_price = response["current_feed_price"]
